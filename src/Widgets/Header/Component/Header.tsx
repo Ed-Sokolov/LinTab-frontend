4035a6e75076e3e95b09f3feecbb654950e30ad0
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-06-25 16:53:47 +0300

    Added Container to Header

--- src/Widgets/Header/Component/Header.tsx
+++ src/Widgets/Header/Component/Header.tsx
@@ -1,5 +1,6 @@
 import {NavLink} from "react-router-dom";
 import './header.scss';
+import {Container} from "../../Container/Container";
 
 export const Header = () => {
     const warningIcon: string = require('../../../Assets/Icons/warning.svg').default
@@ -7,36 +8,40 @@ export const Header = () => {
 
     return (
         <header className="header">
-            <nav className="nav">
-                <ul>
-                    <li>
-                        <NavLink to={'#'}>main</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={'#'}>posts</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={'#'}>projects</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={'#'}>help</NavLink>
-                    </li>
-                </ul>
-            </nav>
-            <div className="warning">
-                <img src={warningIcon} alt=""/>
-            </div>
-            <ul className="auth">
-                <li>
-                    <NavLink to={'#'}>sign in</NavLink>
-                </li>
-                <li>
-                    <img src={lineIcon} alt=""/>
-                </li>
-                <li>
-                    <NavLink to={'#'}>sign up</NavLink>
-                </li>
-            </ul>
+            <Container>
+                <div className="header_component">
+                    <nav className="nav">
+                        <ul>
+                            <li>
+                                <NavLink to={'#'}>main</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'#'}>posts</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'#'}>projects</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'#'}>help</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className="warning">
+                        <img src={warningIcon} alt=""/>
+                    </div>
+                    <ul className="auth">
+                        <li>
+                            <NavLink to={'#'}>sign in</NavLink>
+                        </li>
+                        <li>
+                            <img src={lineIcon} alt=""/>
+                        </li>
+                        <li>
+                            <NavLink to={'#'}>sign up</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </Container>
         </header>
     )
 }
