02267046ca06694a742c9754738db96b0dc5e9cb
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-06-30 13:09:11 +0300

    Modified Footer

--- src/Widgets/Footer/Footer.tsx
+++ src/Widgets/Footer/Footer.tsx
@@ -5,13 +5,37 @@ import {H4} from "../Headings/H4/H4";
 export const Footer = () => {
     return (
         <footer className="footer">
-            <Container>
-                <div className="footer_content">
-                    <div className="company_links">
-                        <H4>Lintab</H4>
+            <div className="footer_content">
+                <Container>
+                    <div className="footer_content_body">
+                        <div className="company_links">
+                            <H4>Lintab</H4>
+                            <ul className="social_list">
+                                <li className="social_item">telegram</li>
+                                <li className="social_item">instagram</li>
+                                <li className="social_item">lintab@gmail.com</li>
+                            </ul>
+                        </div>
+                        <div className="menu">
+                            <p className="menu_title">Menu</p>
+                            <nav className="nav">
+                                <ul className="menu_list">
+                                    <li className="menu_item">Main</li>
+                                    <li className="menu_item">Posts</li>
+                                    <li className="menu_item">Projects</li>
+                                    <li className="menu_item">Help</li>
+                                </ul>
+                            </nav>
+                        </div>
                     </div>
+                </Container>
+                <div className="move_to_contact">
+                    Soon...
                 </div>
-            </Container>
+            </div>
+            <p className="copyright">
+                Copyright © 2023 Sokolov Eduard
+            </p>
         </footer>
     )
 }
