fb298cad9f0555f45931be7d652ea1dfaa845659
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-06-30 14:43:15 +0300

    Added all the icons for a post card

--- src/Widgets/PostCard/PostCard.tsx
+++ src/Widgets/PostCard/PostCard.tsx
@@ -1,6 +1,7 @@
 import "./postCard.scss"
 import {H6} from "../Headings/H6/H6";
 import {UseSvg} from "../UseSvg/UseSvg";
+import {NavLink} from "react-router-dom";
 
 export const PostCard = () => {
     let img = require('../../Assets/Images/PostCard/Test_post_card_image.jpg')
@@ -12,21 +13,32 @@ export const PostCard = () => {
             </div>
             <div className="content">
                 <div className="empty_field"></div>
-                <div className="main_content" data-aos="fade-right">
-                    <ul className="information">
-                        <li className="created_at">06.29.2023</li>
-                        <li className="views">
-                            <UseSvg spriteName="views" className="views_icon"></UseSvg>
-                            <p className="count">100</p>
-                        </li>
-                    </ul>
-                    <H6>Title. Title. Title, title, title, title, title. Title</H6>
-                    <p className="author">by <a href="#" className="move_to_author">author</a></p>
-                    <a href="#" className="move_to_post">Read</a>
+                <div className="main_content_wrapper">
+                    <div className="main_content" data-aos="fade-right">
+                        <ul className="information">
+                            <li className="created_at">06.29.2023</li>
+                            <li className="views">
+                                <UseSvg spriteName="views" className="views_icon"></UseSvg>
+                                <p className="count">100</p>
+                            </li>
+                        </ul>
+                        <H6>Title. Title. Title, title, title, title, title. Title</H6>
+                        <p className="author">by <NavLink to="#" className="move_to_author">author</NavLink></p>
+                        <NavLink to="#" className="move_to_post">
+                            Read
+                            <UseSvg spriteName={"full_arrow_right"} className={"move_to_post_icon"}></UseSvg>
+                        </NavLink>
+                    </div>
                 </div>
                 <ul className="interactions" data-aos="fade-left">
-                    <li className="interaction">100</li>
-                    <li className="interaction">100</li>
+                    <li className="interaction">
+                        <UseSvg spriteName="like" className="interaction_icon"></UseSvg>
+                        <p className="count">100</p>
+                    </li>
+                    <li className="interaction">
+                        <UseSvg spriteName="comment" className="interaction_icon"></UseSvg>
+                        <p className="count">100</p>
+                    </li>
                 </ul>
             </div>
         </article>
