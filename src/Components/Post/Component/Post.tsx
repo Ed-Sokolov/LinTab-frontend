0858fe712247d12a5ee56400918a413efa0b84c1
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-07-01 16:36:38 +0300

    Added like field in the post page

--- src/Components/Post/Component/Post.tsx
+++ src/Components/Post/Component/Post.tsx
@@ -89,6 +89,16 @@ export const Post = () => {
                                 laborum.</p>
                         </div>
                     </div>
+                    <div className="just_like_it_wrapper">
+                        <p className="agitation">Just like it</p>
+                        <div className="pointer">
+                            <UseSvg spriteName={"arrow_circle_down"} className={"pointer_icon"}/>
+                        </div>
+                        <div className="just_like_it">
+                            <UseSvg spriteName={"like"} className={"like_it_icon"}/>
+                            <p className="count">100</p>
+                        </div>
+                    </div>
                 </>
             </Container>
         </div>
