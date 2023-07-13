31a6effa2372a89f7374414ffd526863381ce6a4
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-07-13 20:39:59 +0300

    Added autoComplete to CustomInput component

--- src/Widgets/CustomInput/CustomInput.tsx
+++ src/Widgets/CustomInput/CustomInput.tsx
@@ -14,19 +14,30 @@ type CustomInputTypes = {
     isTouched: boolean | undefined;
 }
 
-export const CustomInput: React.FC<CustomInputTypes> = ({
-                                                            type,
-                                                            errorMessage,
-                                                            name,
-                                                            id,
-                                                            isTouched,
-                                                            placeholder,
-                                                            className = "input",
-                                                            as = "input"
-                                                        }) => {
+export const CustomInput: React.FC<CustomInputTypes> = (
+    {
+        type = "text", errorMessage, name, id, isTouched,
+        placeholder, className = "input", as = "input"
+    }
+) => {
+    let autoCompleteValue: string;
+
+    switch (type) {
+        case "password":
+            autoCompleteValue = "current-password";
+            break;
+        case "email":
+            autoCompleteValue = "email";
+            break;
+        default:
+            autoCompleteValue = "off";
+            break;
+    }
+
     return (
         <div className="input_wrapper">
-            <Field type={type} as={as} id={id} name={name} className={className} placeholder={placeholder}/>
+            <Field type={type} as={as} id={id} name={name} className={className} placeholder={placeholder}
+                   autoComplete={autoCompleteValue}/>
             {(errorMessage && isTouched) && <ErrorField message={errorMessage}/>}
         </div>
     )
