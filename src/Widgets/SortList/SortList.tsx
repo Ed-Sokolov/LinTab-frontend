0607f0b5219e95dae8b0004836954bef386f6241
Ed-Sokolov <ed.sokolov33@gmail.com>
2023-07-21 14:00:45 +0300

    Changed class names in SortList component

--- src/Widgets/SortList/SortList.tsx
+++ src/Widgets/SortList/SortList.tsx
@@ -3,52 +3,52 @@ import {UseSvg} from "../UseSvg/UseSvg";
 
 export const SortList = () => {
     return (
-        <ul className="sort_list" data-aos="fade-left">
-            <li className="sort_item">
-                <ul className="icons_list">
-                    <li className="icon_item">
+        <div className="sort_content" data-aos="fade-left">
+            <div className="sort_list_wrapper">
+                <ul className="sort_list">
+                    <li className="sort_item">
                         <div className="sort_icon_wrapper">
                             <UseSvg spriteName={"sort_title_down"} className={"sort_icon"}/>
                         </div>
                     </li>
-                    <li className="icon_item">
+                    <li className="sort_item">
                         <div className="sort_icon_wrapper">
                             <UseSvg spriteName={"sort_title_up"} className={"sort_icon"}/>
                         </div>
                     </li>
                 </ul>
                 <p className="sort_name">by title</p>
-            </li>
-            <li className="sort_item">
-                <ul className="icons_list">
-                    <li className="icon_item active">
+            </div>
+            <div className="sort_list_wrapper">
+                <ul className="sort_list">
+                    <li className="sort_item active">
                         <div className="sort_icon_wrapper">
                             <UseSvg spriteName={"sort_date_down"} className={"sort_icon"}/>
                         </div>
                     </li>
-                    <li className="icon_item">
+                    <li className="sort_item">
                         <div className="sort_icon_wrapper">
                             <UseSvg spriteName={"sort_date_up"} className={"sort_icon"}/>
                         </div>
                     </li>
                 </ul>
                 <p className="sort_name">by date</p>
-            </li>
-            <li className="sort_item">
-                <ul className="icons_list">
-                    <li className="icon_item">
+            </div>
+            <div className="sort_list_wrapper">
+                <ul className="sort_list">
+                    <li className="sort_item">
                         <div className="sort_icon_wrapper">
                             <UseSvg spriteName={"sort_views_down"} className={"sort_icon"}/>
                         </div>
                     </li>
-                    <li className="icon_item">
+                    <li className="sort_item">
                         <div className="sort_icon_wrapper">
                             <UseSvg spriteName={"sort_views_up"} className={"sort_icon"}/>
                         </div>
                     </li>
                 </ul>
                 <p className="sort_name">by views</p>
-            </li>
-        </ul>
+            </div>
+        </div>
     )
 }
